77feed33e5cf692da344c74ef0bc298ed4b62c76
Marko Corn <marko.corn@gmail.com>
2017-03-25 11:44:13 +0100

    Transfer nodes testing from cell

--- src/main/java/ameba/core/reproductions/crossCell/TransferNodes.java
+++ src/main/java/ameba/core/reproductions/crossCell/TransferNodes.java
@@ -1,6 +1,7 @@
 package ameba.core.reproductions.crossCell;
 
 import ameba.core.blocks.Cell;
+import ameba.core.blocks.collectors.CollectorSource;
 import ameba.core.blocks.edges.Edge;
 import ameba.core.blocks.nodes.Node;
 import ameba.core.factories.FactoryCell;
@@ -89,65 +90,12 @@ public class TransferNodes implements ICrossCell {
         reconnectEdgesInp(cell1, borderEdges.get("edgesInpInt"), borderEdges2.get("edgesInpInt"));
         reconnectEdgesInp(cell1, borderEdges.get("edgesInpBin"), borderEdges2.get("edgesInpBin"));
 
-        reconnectEdgesOut(borderEdges.get("edgesOutDec"), borderEdges2.get("edgesOutDec"));
-        reconnectEdgesOut(borderEdges.get("edgesOutInt"), borderEdges2.get("edgesOutInt"));
-        reconnectEdgesOut(borderEdges.get("edgesOutBin"), borderEdges2.get("edgesOutBin"));
+        reconnectEdgesOut(cell1, borderEdges.get("edgesOutDec"), borderEdges2.get("edgesOutDec"));
+        reconnectEdgesOut(cell1, borderEdges.get("edgesOutInt"), borderEdges2.get("edgesOutInt"));
+        reconnectEdgesOut(cell1, borderEdges.get("edgesOutBin"), borderEdges2.get("edgesOutBin"));
 
         cellFactory.connectsMinFreeInputs(cell1);
-        if (cell1.checkCell().size() > 0) {
-            //Remove group of nodes with edges from cell1
-            group = cell3.getGroup(cell3.getInnerNodes().get(random.nextInt(cell3.getInnerNodes().size())), random.nextInt(maxNodes) + 1);
-            borderEdges = cell3.getGroupEdgesBorder(group);
-            innerEdges = cell3.getGroupEdgesInner(group);
-            //Remove inner edges
-            for (Edge edge : innerEdges.get("edgesDec")) {
-                cell3.removeEdge(edge);
-            }
-            for (Edge edge : innerEdges.get("edgesInt")) {
-                cell3.removeEdge(edge);
-            }
-            for (Edge edge : innerEdges.get("edgesBin")) {
-                cell3.removeEdge(edge);
-            }
-            //Remove nodes
-            for (ArrayList<Node> nodes : group) {
-                for (Node node : nodes) {
-                    cell3.removeNode(node);
-                }
-            }
 
-            //Add group of nodes and edges from cell2 to cell 1
-            group2 = cell4.getGroup(cell4.getInnerNodes().get(random.nextInt(cell4.getInnerNodes().size())), random.nextInt(maxNodes - 1) + 2);
-            borderEdges2 = cell4.getGroupEdgesBorder(group2);
-            innerEdges2 = cell4.getGroupEdgesInner(group2);
-            //Add new nodes to cell 1
-            for (ArrayList<Node> nodes : group2) {
-                for (Node node : nodes) {
-                    cell4.addNode(node);
-                }
-            }
-            //Add inner edges
-            for (Edge edge : innerEdges2.get("edgesDec")) {
-                cell4.addEdgeNoVerification(edge);
-            }
-            for (Edge edge : innerEdges2.get("edgesInt")) {
-                cell4.addEdgeNoVerification(edge);
-            }
-            for (Edge edge : innerEdges2.get("edgesBin")) {
-                cell4.addEdgeNoVerification(edge);
-            }
-
-            //Reconnect edges
-            reconnectEdgesInp(cell3, borderEdges.get("edgesInpDec"), borderEdges2.get("edgesInpDec"));
-            reconnectEdgesInp(cell3, borderEdges.get("edgesInpInt"), borderEdges2.get("edgesInpInt"));
-            reconnectEdgesInp(cell3, borderEdges.get("edgesInpBin"), borderEdges2.get("edgesInpBin"));
-
-            reconnectEdgesOut(borderEdges.get("edgesOutDec"), borderEdges2.get("edgesOutDec"));
-            reconnectEdgesOut(borderEdges.get("edgesOutInt"), borderEdges2.get("edgesOutInt"));
-            reconnectEdgesOut(borderEdges.get("edgesOutBin"), borderEdges2.get("edgesOutBin"));
-
-            cellFactory.connectsMinFreeInputs(cell1);
-        }
         return cell1;
     }
 
@@ -177,21 +125,23 @@ public class TransferNodes implements ICrossCell {
         }
     }
 
-    private void reconnectEdgesOut(ArrayList<Edge> borderEdges1, ArrayList<Edge> borderEdges2) throws Exception {
+    private void reconnectEdgesOut(Cell cell, ArrayList<Edge> borderEdges1, ArrayList<Edge> borderEdges2) throws Exception {
         int diff = borderEdges1.size() - borderEdges2.size();
         int same = Math.min(borderEdges1.size(), borderEdges2.size());
         Collections.shuffle(borderEdges2);
         for (int i = 0; i < same; i++) {
             Edge e1 = borderEdges1.get(i);
             Edge e2 = borderEdges2.get(i);
-            e1.setSource(e2.getSource());
-            e2.getSource().removeEdge(e2);
-            e2.getSource().addEdge(e1);
+            CollectorSource s = e2.getSource();
+            e1.setSource(s);
+            s.removeEdge(e2);
+            s.addEdge(e1);
             System.out.println("4");
         }
         if (diff > 0) {
             for (int i = same; i < same - diff; i++) {
-                borderEdges1.get(i).getTarget().removeEdge(borderEdges2.get(i));
+                borderEdges1.get(i).getTarget().removeEdge(borderEdges1.get(i));
+                cell.removeEdge(borderEdges1.get(i));
                 System.out.println("5");
             }
         } else {
---
+++ src/test/java/rep3.java
@@ -0,0 +1,126 @@
+import ameba.core.blocks.Cell;
+import ameba.core.blocks.edges.Edge;
+import ameba.core.blocks.nodes.types.*;
+import ameba.core.factories.*;
+import ameba.core.reproductions.crossCell.TransferNodes;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.util.Arrays;
+
+/**
+ * Created by marko on 10/24/16.
+ */
+public class rep3 {
+    public static void main(String[] args) throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode jsonSettings = mapper.readTree(new File("/home/marko/IdeaProjects/ameba-core/src/main/resources/settings.json"));
+        FactoryNode factoryNode = new FactoryNode();
+        factoryNode.loadSettings(jsonSettings.get("nodeFactorySettings").toString());
+        FactoryEdge factoryEdge = new FactoryEdge(mapper.readValue(jsonSettings.get("edgeFactorySettings").toString(), FactoryEdgeSettings.class));
+        FactoryCell factoryCell = new FactoryCell(mapper.readValue(jsonSettings.get("cellFactorySettings").toString(), FactoryCellSettings.class), factoryNode, factoryEdge);
+
+
+        factoryCell.getCellFactorySettings().setNodeInpDec(1);
+        factoryCell.getCellFactorySettings().setNodeInpInt(1);
+        factoryCell.getCellFactorySettings().setNodeInpBin(1);
+        factoryCell.getCellFactorySettings().setNodeOutDec(1);
+        factoryCell.getCellFactorySettings().setNodeOutInt(1);
+        factoryCell.getCellFactorySettings().setNodeOutBin(1);
+        factoryCell.getCellFactorySettings().setNodeInitial(new Integer[]{2, 2});
+
+        TransferNodes transferNodes = new TransferNodes(factoryNode, factoryCell, factoryEdge, 5);
+
+        double[][] inpDec = new double[][]{{0.0}, {1.0}, {2.0}, {3.0}, {4.0}, {5.0}, {6.0}, {7.0}, {8.0}, {9.0}};
+        int[][] inpInt = new int[][]{{0}, {1}, {-1}, {2}, {-2}, {3}, {-3}, {4}, {-4}, {5}};
+        boolean[][] inpBin = new boolean[][]{{false}, {false}, {true}, {true}, {true}, {false}, {false}, {true}, {true}, {true}};
+
+        double[][] outDec = new double[10][1];
+        int[][] outInt = new int[10][1];
+        boolean[][] outBin = new boolean[10][1];
+
+        Cell cell = new Cell(10);
+        cell.addNode(new InputDec());
+        cell.addNode(new InputInt());
+        cell.addNode(new InputBin());
+        cell.addNode(new OutputDec());
+        cell.addNode(new OutputInt());
+        cell.addNode(new OutputBin());
+
+        cell.addNode(factoryNode.genNode("SwitchConstDec"));
+        cell.addNode(factoryNode.genNode("IntegralInt"));
+        cell.addEdge(new Edge(cell.getNodes().get(0), cell.getNodes().get(6), ));
+
+
+        try {
+            for (int i = 0; i < 1000; i++) {
+                System.out.println(i + " Run number");
+                Cell cell = factoryCell.genCellRnd();
+                try {
+                    cell = factoryCell.genCellRnd();
+                    cell = transferNodes.cross(cell, cell.clone());
+//                    cell = removeNodesGroup.mutate(cell);
+                    if (cell.checkCell().size() > 0) {
+                        int t = 0;
+                    }
+                    cell.checkCellPrint();
+                    for (int j = 0; j < inpDec.length; j++) {
+                        cell.runEvent(inpDec[j], inpInt[j], inpBin[j]);
+                        outBin[j] = cell.getExportedValuesBin();
+                        outInt[j] = cell.getExportedValuesInt();
+                        outDec[j] = cell.getExportedValuesDec();
+                    }
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+                for (int k = 0; k < outBin.length; k++) {
+                    System.out.println(Arrays.toString(outDec[k]) + " : " + Arrays.toString(outInt[k]) + " : " + Arrays.toString(outBin[k]));
+                }
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+//        ArrayList<ArrayList<Signal>> inputs = new ArrayList<>();
+//        for (int i = 0; i < 10; i++) {
+//            inputs.add(new ArrayList(Arrays.asList(Signal.createDouble(i), Signal.createInteger(i - 5), Signal.createBoolean(true))));
+//        }
+//
+//        try {
+//            Cell cell = factoryCell.genCellRnd();
+//            Cell cell1 = cell;
+//            for (int i = 0; i < 100; i++) {
+//                System.out.println(i + " Run number");
+//
+//                try {
+////                    cell1 = replaceNode.mutate(cell);
+////                    cell1 = addNodesGroup.mutate(factoryCell.genCellRnd());
+//                    cell1 = addNodesGroup.mutate(cell1);
+//                } catch (Exception ex) {
+//                    ex.printStackTrace();
+//                    cell1 = addNodesGroup.mutate(cell1);
+//                }
+//
+//                ArrayList<String> test = cell1.checkCell();
+//                System.out.println(test);
+//                ArrayList<ArrayList<Signal>> outputs = new ArrayList<>();
+//                try {
+//                    outputs = cell1.run(inputs);
+//                } catch (Exception ex) {
+//                    ex.printStackTrace();
+//                }
+//                for (ArrayList<Signal> out : outputs) {
+//                    for (Signal obj : out) {
+//                        System.out.print(obj.getValue());
+//                        System.out.print(":");
+//                    }
+//                    System.out.println();
+//                }
+//
+//            }
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+    }
+}
