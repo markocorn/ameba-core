d43604c6e85443205b7fe186ec705f81dfad8f49
Marko Corn <marko.corn@gmail.com>
2017-03-30 19:46:21 +0200

    Reproduction factory cell generation

---
+++ src/main/java/ameba/core/reproductions/mutateEdge/IMutateEdge.java
@@ -0,0 +1,10 @@
+package ameba.core.reproductions.mutateEdge;
+
+import ameba.core.blocks.Cell;
+
+/**
+ * Created by marko on 3/30/17.
+ */
+public interface IMutateEdge {
+    public Cell.Signal getEdgeType();
+}
